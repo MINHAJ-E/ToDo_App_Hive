97f31e812232467e409bebac321e5b5029d986f6
MINHAJ-E <minhaj1389@gmail.com>
2023-09-06 23:19:19 -0700

    littile changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:todo_hive/model/model.dart';
-import 'package:todo_hive/todo_list.dart';
+import 'package:todo_hive/screens/todo_list.dart';
 // import 'package:todo_hive/todopage.dart';
 
 Future<void> main()async{
